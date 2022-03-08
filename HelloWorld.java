486f3035e6272156c1b86c5cdb5ac7cf4e2162d5
John Sheehan <sheehanj@sunyulster.edu>
2022-03-08 13:39:54 -0500

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+package helloworld;
+
+// John Sheehan
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/* Prints Hello World! */
+		int temp = 100;
+
+		if (temp > 90) {
+		   System.out.println("This porridge is too hot.");
+		}
+		 
+		else if (temp < 110) {
+		   System.out.println("This porridge is too cold.");
+		}
+
+		if (temp == 80) {
+		   System.out.println("This porridge is just right!");
+		}
+	}
+
+}
